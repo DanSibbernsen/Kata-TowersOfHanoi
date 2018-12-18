f0003b03c5c650356f3d23f33d74039705171f64
dan.sibbernsen <sibbernsen.dan@gmail.com>
2018-12-18 16:57:07 -0600

    -adding in number of moves to verify solution. Condensing code to remove base case.

--- TowersOfHanoi/ruby/solution.rb
+++ TowersOfHanoi/ruby/solution.rb
@@ -1,17 +1,16 @@
+$number_of_moves = 0
 def solution (number_of_disks, source, destination, spare)
-  if number_of_disks == 0
-    disk = source.pop
-    destination.push(disk)
-  else
+    if number_of_disks > 0
     solution(number_of_disks-1, source, spare, destination)
+    $number_of_moves += 1
     disk = source.pop
     destination.push(disk)
     solution(number_of_disks-1, spare, destination, source)
   end
 end
 
-number_of_disks = 5
-towerBeginning = (0..number_of_disks).to_a
+number_of_disks = 3
+towerBeginning = (1..number_of_disks).to_a
 towerMiddle = []
 towerEnd = []
 puts "Before solution:"
@@ -28,3 +27,5 @@ puts
 print "spare: #{towerMiddle}"
 puts
 print "destination: #{towerEnd}"
+puts
+puts "Number of moves: #{$number_of_moves}"
